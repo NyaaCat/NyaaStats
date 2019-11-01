93ea5095f6f9f5f16463ee28046366df7c9e171f
SilentDepth <silentdepth@outlook.com>
2019-11-01 17:15:00 +0800

    feat: add advancement frames

---
+++ web/src/assets/frames/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export const challenge_complete = require('./challenge_complete.png');
+export const challenge = require('./challenge.png');
+export const goal_complete = require('./goal_complete.png');
+export const goal = require('./goal.png');
+export const normal_complete = require('./normal_complete.png');
+export const normal = require('./normal.png');
--- web/src/components/AdvancementBlock.vue
+++ web/src/components/AdvancementBlock.vue
@@ -4,7 +4,9 @@
       <div class="panel-body">
         <div class="media">
           <div class="media-left">
-            <img class="media-object achievements" :src="getImg" />
+            <div class="advancement-frame" :style="{backgroundImage: `url(${frame})`}">
+              <img class="media-object achievements" :src="icon">
+            </div>
           </div>
           <div class="media-body text-middle">
             <h4 class="media-heading">
@@ -29,8 +31,10 @@
 </template>
 
 <script>
+import advancementsData from '../assets/advancements.json';
 import lang from '../assets/lang.json';
 import { advancements } from '../assets/advancements';
+import * as frames from '../assets/frames';
 
 export default {
   name: 'AdvancementBlock',
@@ -41,10 +45,30 @@ export default {
     };
   },
   computed: {
-    getImg() {
+    frame() {
+      return frames[advancementsData[this.adv.advId].type]
+    },
+    icon() {
       return advancements[this.adv.type][this.adv.adv];
     },
   },
 };
 
 </script>
+
+<style>
+  .advancement-frame {
+    width: 52px;
+    height: 52px;
+    position: relative;
+  }
+
+  .advancement-frame img {
+    position: absolute;
+    margin: auto;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+</style>
