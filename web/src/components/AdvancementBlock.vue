0aef046cefb1c6f3f7ecb60f492b6a3b6f3803fd
SilentDepth <silentdepth@outlook.com>
2019-11-07 23:54:12 +0800

    feat: add advancement progress detail

--- web/src/components/AdvancementBlock.vue
+++ web/src/components/AdvancementBlock.vue
@@ -1,25 +1,122 @@
 <template>
   <div class="advancement-block col-sm-12 col-md-6 col-lg-4">
     <AdvancementTitle :advancement-id="adv.advId" />
+    <div
+      v-if="requirements.length > 1"
+      class="advancement-requirements relative"
+    >
+      <AdvancementDescription
+        class="advancement-requirements__inner absolute w-full"
+      >
+        <p
+          v-for="req of requirements"
+          :key="req"
+          :class="achieved.includes(req) && 'done'"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path :d="achieved.includes(req) | svgPath" />
+          </svg>
+          {{ lang(advData.requirement_names[req]) }}
+        </p>
+      </AdvancementDescription>
+    </div>
   </div>
 </template>
 
 <script>
+import advancementData from '@/assets/advancement-data'
 import AdvancementTitle from './AdvancementTitle'
+import AdvancementDescription from './AdvancementDescription'
 
 export default {
   name: 'AdvancementBlock',
 
   components: {
     AdvancementTitle,
+    AdvancementDescription,
+  },
+
+  filters: {
+    svgPath: val =>
+      val
+        ? 'M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z'
+        : 'M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z',
   },
 
   props: ['adv'],
+
+  computed: {
+    advData() {
+      return advancementData[this.adv.advId]
+    },
+
+    requirements() {
+      return this.advData.requirements || []
+    },
+
+    achieved() {
+      const uuid = this.$route.params.uuid
+      const player = this.$store.state.players[uuid].advancements[
+        this.adv.advId
+      ]
+      return Object.keys(player.criteria)
+    },
+  },
 }
 </script>
 
 <style>
 .advancement-block {
   margin-bottom: 28px;
+}
+
+.advancement-requirements {
+  top: -14px;
+  z-index: 1;
+}
+
+.advancement-requirements__inner {
+  display: none;
+  color: #fff;
+  line-height: 1;
+}
+
+.advancement-requirements__inner p {
+  margin: 2px 0;
+  color: #aaa;
+  font-size: 17px;
+  display: flex;
+  align-items: center;
+}
+
+.advancement-requirements__inner p svg {
+  width: 1em;
+  height: 1em;
+}
+
+.advancement-requirements__inner p.done {
+  color: #fff;
+}
+
+.advancement-requirements__inner p.done svg {
+  fill: green;
+}
+
+.advancement-requirements__inner p:not(.done) svg {
+  fill: red;
+}
+
+.advancement-block:hover .advancement-title {
+  z-index: 2;
+}
+
+.advancement-block:hover .advancement-requirements__inner {
+  display: block;
 }
 </style>
---
+++ web/src/components/AdvancementDescription.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="advancement-description">
+    <div class="upper outside"></div>
+    <div class="upper inside"></div>
+    <div class="middle outside">
+      <div class="middle inside">
+        <slot />
+      </div>
+    </div>
+    <div class="lower inside"></div>
+    <div class="lower outside"></div>
+  </div>
+</template>
+
+<style>
+.advancement-description .upper.outside,
+.advancement-description .lower.outside {
+  margin: 0 4px;
+  height: 2px;
+  background: #000;
+}
+
+.advancement-description .upper.inside,
+.advancement-description .lower.inside {
+  margin: 0 2px;
+  height: 2px;
+  background: #555;
+  border-width: 0 2px;
+  border-style: solid;
+  border-color: #000;
+}
+
+.advancement-description .middle.outside {
+  border-width: 0 2px;
+  border-style: solid;
+  border-color: #000;
+}
+
+.advancement-description .middle.inside {
+  border-width: 0 2px;
+  border-style: solid;
+  border-color: #555;
+  background: #212121;
+  padding: 10px 4px 4px;
+  font-size: 17px;
+}
+</style>
