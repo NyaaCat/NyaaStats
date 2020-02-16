5b18cd6ff0e3c1978bc0baf6cf0df1d2bfba9da7
SilentDepth <silentdepth@outlook.com>
2020-02-17 03:52:46 +0800

    feat(web): add overview stats to server info section

--- web/src/components/Welcome.vue
+++ web/src/components/Welcome.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="jumbotron">
     <div class="container">
-      <h1>{{ info.servername }}</h1>
-      <p>Server time elapsed {{ worldTime }}</p>
-
-      <p>
-        <a :href="homePage" role="button" class="btn btn-primary btn-lg">Learn more &raquo;</a>
-      </p>
+      <div class="text-center">
+        <h1>{{ info.servername }}</h1>
+        <a :href="info.homepage" class="btn btn-link btn-lg inline-flex items-center">
+          Learn More
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8z" /></svg>
+        </a>
+      </div>
+      <dl class="flex items-start">
+        <div class="flex-1 flex flex-col-reverse text-center">
+          <dt class="font-normal text-gray-600">World Running Time</dt>
+          <dd class="font-bold">{{ worldTime }}</dd>
+        </div>
+        <div class="flex-1 flex flex-col-reverse text-center">
+          <dt class="font-normal text-gray-600">Total Players</dt>
+          <dd class="font-bold">{{ totalPlayers }}</dd>
+        </div>
+        <div class="flex-1 flex flex-col-reverse text-center">
+          <dt class="font-normal text-gray-600">Recently Active Players</dt>
+          <dd class="font-bold">{{ activePlayers }}</dd>
+        </div>
+      </dl>
     </div>
   </div>
 </template>
@@ -17,14 +32,49 @@
 
   export default {
     name: 'Welcome',
+
     computed: {
       ...mapState(['info']),
-      worldTime() {
-        return moment.duration(this.info.worldTime || 0, 'seconds').humanize()
+
+      worldTime () {
+        return this.info.worldTime ? moment.duration(this.info.worldTime, 'seconds').humanize() : '--'
       },
-      homePage() {
-        return this.info.homepage
+
+      totalPlayers () {
+        return this.$store.state.playerList.length || '--'
+      },
+
+      activePlayers () {
+        const _lastUpdate = this.info.lastUpdate
+        return this.$store.state.playerList.filter(p => _lastUpdate - p.seen <= 24 * 60 * 60 * 1000).length || '--'
       },
     },
   }
 </script>
+
+<style scoped>
+  svg {
+    width: 1em;
+    height: 1em;
+    fill: currentColor;
+    margin-left: 8px;
+  }
+
+  dt {
+    font-size: 16px;
+  }
+
+  dd {
+    font-size: 20px;
+  }
+
+  @media (min-width: 768px) {
+    dt {
+      font-size: 20px;
+    }
+
+    dd {
+      font-size: 40px;
+    }
+  }
+</style>
