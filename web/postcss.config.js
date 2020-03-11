417d5491261ab30719066d447b6714fafc71174e
SilentDepth <silentdepth@outlook.com>
2020-03-12 01:33:56 +0800

    build(web): use purgecss to reduce css size

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,15 @@
+const purgecssOptions = {
+  content: [
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+}
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...process.env.NODE_ENV === 'production' ? {'@fullhuman/postcss-purgecss': purgecssOptions} : {},
   },
 }
