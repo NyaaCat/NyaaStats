14051761ab67fe87c5ecc06f1891cf1a9eeb4022
SilentDepth <silentdepth@outlook.com>
2020-03-17 13:56:03 +0800

    chore(web): save navbar bg img to local

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="set-height">
-    <div class="page-section set-bg bg-black text-gray-300 fixed inset-x-0 top-0">
+    <div class="page-section set-bg text-gray-300 fixed inset-x-0 top-0">
       <div class="set-height flex items-center">
         <RouterLink to="/" class="font-medium hover:text-white transition-color duration-100 ease-linear">{{ info.title }}</RouterLink>
         <div class="ml-auto">
@@ -33,6 +33,6 @@
   }
 
   .set-bg {
-    background: url("https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/header/bg-wool-dark.png") repeat;
+    background: #1c1c1c url("../assets/bg-wool-dark.png") repeat;
   }
 </style>
