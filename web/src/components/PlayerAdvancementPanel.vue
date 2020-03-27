ace6af7cea791623d7d2d0f7c2673b20da24c812
SilentDepth <silentdepth@outlook.com>
2020-03-28 04:00:32 +0800

    chore(web): please eslint

--- web/src/components/PlayerAdvancementPanel.vue
+++ web/src/components/PlayerAdvancementPanel.vue
@@ -29,9 +29,9 @@
       </h3>
       <div class="px-page xl:px-4 py-2 lg:flex lg:flex-wrap lg:-ml-5">
         <div
-          v-for="(adv, idx) of data"
+          v-for="(adv, advIdx) of data"
           :key="adv.id"
-          :class="['lg:flex-grow-0 lg:flex-shrink lg:w-1/2 xl:w-1/3 lg:pl-5 py-1 md:overflow-visible transition-opacity duration-100 ease-linear', {'opacity-50': openedAdv && openedAdv !== adv.id, 'second': idx % 2 === 1, 'third': idx % 3 === 2}]"
+          :class="['lg:flex-grow-0 lg:flex-shrink lg:w-1/2 xl:w-1/3 lg:pl-5 py-1 md:overflow-visible transition-opacity duration-100 ease-linear', {'opacity-50': openedAdv && openedAdv !== adv.id, 'second': advIdx % 2 === 1, 'third': advIdx % 3 === 2}]"
         >
           <AdvancementTitle
             v-show="openedAdv !== adv.id"
