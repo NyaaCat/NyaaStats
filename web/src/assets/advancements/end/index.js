76d8f80539801a07ae08734f23af5d02a8632893
SilentDepth <silentdepth@outlook.com>
2021-12-04 11:50:22 +0800

    feat(web): add 1.18 data

--- web/src/assets/advancements/end/index.js
+++ web/src/assets/advancements/end/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
-export const root = require('./root.png');
-export const kill_dragon = require('./kill_dragon.png');
-export const dragon_egg = require('./dragon_egg.png');
-export const enter_end_gateway = require('./enter_end_gateway.png');
-export const respawn_dragon = require('./respawn_dragon.gif');
 export const dragon_breath = require('./dragon_breath.png');
-export const find_end_city = require('./find_end_city.png');
+export const dragon_egg = require('./dragon_egg.png');
 export const elytra = require('./elytra.png');
+export const enter_end_gateway = require('./enter_end_gateway.png');
+export const find_end_city = require('./find_end_city.png');
+export const kill_dragon = require('./kill_dragon.png');
 export const levitate = require('./levitate.png');
+export const respawn_dragon = require('./respawn_dragon.gif');
+export const root = require('./root.png');
